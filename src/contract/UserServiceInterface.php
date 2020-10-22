8c803289569c2f08392923a9fee2e67f6d73b04d
蔡嘉俊 <caijiajun@caijiajundeMacBook-Pro.local>
2020-10-22 12:55:49 +0800

    first commit

---
+++ src/contract/UserServiceInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of jyhf-rpc-server.
+ *
+ * @link     http://www.zjjyhf.com
+ * @contact  253618519@qq.com
+ */
+
+namespace Jyhf\RpcServer;
+
+interface UserServiceInterface
+{
+    /**
+     * Register user
+     * @param string      $username
+     * @param string      $password
+     * @param string|null $parent
+     *
+     * @return bool
+     */
+    public function register(string $username, string $password, string $parent = null): bool;
+}
